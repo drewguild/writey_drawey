6b60f1894e77399f488a7435dafbe41b25ab5235
drewguild <drew.guild@gmail.com>
2020-04-08 22:44:17 -0400

    store state is session vs local

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -2,7 +2,7 @@
 
 export function loadState() {
   try {
-    const serializedState = localStorage.getItem('state')
+    const serializedState = sessionStorage.getItem('state')
 
     if (serializedState === null) {
       return undefined
@@ -16,7 +16,7 @@ export function loadState() {
 export function saveState(state) {
   try {
     const serializedState = JSON.stringify(state)
-    localStorage.setItem('state', serializedState)
+    sessionStorage.setItem('state', serializedState)
   } catch (err) {
     // Ignore... for now
   }
